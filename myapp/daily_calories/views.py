b5817f4781e28bde096736634c3b29aa21168cb5
Fabian Silva <silvafabian74@yahoo.com>
2022-04-11 15:29:33 -0400

    add update route

--- myapp/daily_calories/views.py
+++ myapp/daily_calories/views.py
@@ -18,7 +18,8 @@ def create_post():
       user_id=current_user.id,
       breakfast=form.string.data,
       lunch=form.string.data,
-      dinner=form.string.data
+      dinner=form.string.data,
+      notes=form.string.data
     )
     db.session.add(daily_calories)
     db.session.commit()
@@ -26,3 +27,44 @@ def create_post():
     print('Calorie post was created')
     return redirect(url_for('core.index'))
   return render_template('create_post.html', form=form)
+
+@daily_calories.route('/<int:daily_calories_id>')
+def daily_calories(daily_calories_id):
+  daily_calories = DailyCalories.query.get_or_404(daily_calories_id) 
+  return render_template(
+    'daily_calories.html', 
+    title=daily_calories.title, 
+    date=daily_calories.date,
+    breakfast=daily_calories.breakfast, 
+    lunch=daily_calories.lunch,
+    dinner=daily_calories.dinner, 
+    notes=daily_calories.notes, 
+    post=daily_calories
+  )
+
+@daily_calories.route('/<int:daily_calorie_id>/update',methods=['GET','POST'])
+@login_required
+def update(daily_calorie_id):
+  daily_calorie = DailyCalories.query.get_or_404(daily_calorie_id)
+  if daily_calorie.author != current_user:
+    abort(403)
+  form = DailyCaloriesForm()
+  if form.validate_on_submit():
+    daily_calorie.title = form.title.data
+    daily_calorie.text = form.text.data
+    daily_calorie.breakfast = form.string.data
+    daily_calorie.lunch = form.string.data
+    daily_calorie.dinner = form.string.data
+    daily_calorie.notes = form.text.data
+    db.session.commit()
+    flash('Calories Updated')
+    return redirect(url_for('daily_calories.daily_calorie',daily_calorie_id=daily_calorie.id))
+  elif request.method == 'GET':
+    form.title.data = daily_calorie.title
+    form.text.data = daily_calorie.text
+    form.text.data = daily_calorie.notes
+    form.string.data = daily_calorie.breakfast
+    form.string.data = daily_calorie.lunch
+    form.string.data = daily_calorie.dinner
+
+  return render_template('create_post.html',title='Updating Calories',form=form)
--- myapp/users/views.py
+++ myapp/users/views.py
@@ -29,19 +29,13 @@ def login():
 
   form = LoginForm()
   if form.validate_on_submit():
-
     user = User.query.filter_by(email=form.email.data).first()
-
     if user.check_password(form.password.data) and user is not None:
-
       login_user(user)
       flash('Log in Success!')
-
       next = request.args.get('next')
-
       if next ==None or not next[0]=='/':
-          next = url_for('core.index')
-
+        next = url_for('core.index')
       return redirect(next)
 
   return render_template('login.html',form=form)
