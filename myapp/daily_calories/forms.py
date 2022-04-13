c55a1847cc7f4b138e1d82f713ae5b43f2917dd8
Fabian Silva <silvafabian74@yahoo.com>
2022-04-13 11:09:39 -0400

    remove datefeild due to errors

--- myapp/daily_calories/forms.py
+++ myapp/daily_calories/forms.py
@@ -1,10 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, IntegerField
 from wtforms.validators import DataRequired
-from wtforms.fields.html5 import DateField
 
 class DailyCaloriesForm(FlaskForm):
-  title = DateField('Date', format='%Y-%m-%d')
+  title = StringField('Date', validators=[DataRequired()])
   notes = TextAreaField('Notes', validators=[DataRequired()])
   breakfast = StringField('Breakfast', validators=[DataRequired()])
   breakfast_calories = IntegerField('Breakfast Calories', validators=[DataRequired()])
