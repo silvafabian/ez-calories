10a54e9a8253939233602ee2c122a41fd8b3fe44
Fabian Silva <silvafabian74@yahoo.com>
2022-04-13 10:32:35 -0400

    add calender to form data

--- myapp/daily_calories/forms.py
+++ myapp/daily_calories/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, IntegerField
 from wtforms.validators import DataRequired
+from wtforms.fields.html5 import DateField
 
 class DailyCaloriesForm(FlaskForm):
-  title = StringField('Title', validators=[DataRequired()])
-  notes = TextAreaField('Text', validators=[DataRequired()])
+  title = DateField('Date', format='%Y-%m-%d')
+  notes = TextAreaField('Notes', validators=[DataRequired()])
   breakfast = StringField('Breakfast', validators=[DataRequired()])
   breakfast_calories = IntegerField('Breakfast Calories', validators=[DataRequired()])
   lunch = StringField('Lunch', validators=[DataRequired()])
